4a683d0accff39e65735dd4cadba6da38cd1bbe2
Sujal Chauhan <sujalchauhan826@gmail.com>
2026-02-01 21:57:30 +0530

    Updated website

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
           <Route path="/articles" element={<Articles />} />
+          <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
